e201b8336a58b115f24380d1dfe7797b1c998655
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-06-09 10:24:39 +0530

    Update page.js

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -6,17 +6,17 @@ export default async function BlogsDefaultPage() {
   const pageIndex = 1;
   const perPage = 2;
 
-  const allBlogs = await getAllBlogs("all", "all"); 
-  console.log(allBlogs)
+  const allBlogs = await getAllBlogs(); 
+  console.log(allBlogs.blogs)
 //   if (!allProducts || allProducts.length === 0) return notFound();
 
   const totalPages = Math.ceil(allBlogs.length / perPage);
   const start = (pageIndex - 1) * perPage;
   const end = start + perPage;
-  const paginatedBlogs = allBlogs.slice(start, end);
+  const paginatedBlogs = allBlogs.blogs.slice(start, end);
 
   const slugPath = [
-    { slug_name: "All Products", slug: "/all-products" },
+    { slug_name: "Blogs", slug: "/blogs/" },
   ];
 
   return (
