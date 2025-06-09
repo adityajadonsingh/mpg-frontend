a8fa8605d37a45cada08d3274bc9d267e7af6e63
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-06-09 11:18:47 +0530

    Blogs page

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,25 +1,61 @@
+import Breadcrum from "@/components/Breadcrum";
+import MiniBanner from "@/components/MiniBanner";
 import { getAllBlogs } from "@/lib/api/blogs";
-import { notFound } from "next/navigation";
-import AllProductsClientPage from "@/components/product/AllProductsClientPage";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function BlogsDefaultPage() {
-  const pageIndex = 1;
-  const perPage = 2;
+    const pageIndex = 1;
+    const perPage = 2;
 
-  const allBlogs = await getAllBlogs(); 
-  console.log(allBlogs.blogs)
-//   if (!allProducts || allProducts.length === 0) return notFound();
+    const allBlogs = await getAllBlogs();
+    console.log(allBlogs.blogs)
 
-  const totalPages = Math.ceil(allBlogs.length / perPage);
-  const start = (pageIndex - 1) * perPage;
-  const end = start + perPage;
-  const paginatedBlogs = allBlogs.blogs.slice(start, end);
+    const totalPages = Math.ceil(allBlogs.length / perPage);
+    const start = (pageIndex - 1) * perPage;
+    const end = start + perPage;
+    const paginatedBlogs = allBlogs.blogs.slice(start, end);
 
-  const slugPath = [
-    { slug_name: "Blogs", slug: "/blogs/" },
-  ];
+    const slugPath = [
+        { slug_name: "Blogs", slug: "/blogs/" },
+    ];
 
-  return (
-    <></>
-  );
+    return (
+        <>
+            <MiniBanner bg_img={"https://img.freepik.com/premium-photo/background-texture-natural-stone-marble-granite-mosaic_166216-235.jpg"} pageName={"All Blogs"} />
+            <Breadcrum path_arr={slugPath} />
+            <section className="blog-page">
+                <div className="wrapper">
+                    <div className="grid lg:grid-cols-3 gap-5">
+                        {
+                            allBlogs.blogs.map((blog, idx) => {
+                                return <Link key={`blog-${idx}`} href={`/blogs/${blog.slug}`} className="block">
+                                    <div className="bg-gray-100 rounded-xl overflow-hidden shadow-md hover:shadow-lg transition-all h-full">
+                                        <div className="img-box relative">
+                                            <Image
+                                                src={blog.image}
+                                                alt={blog.title}
+                                                fill
+                                                style={{ objectFit: "cover" }}
+                                            />
+                                        </div>
+                                        <div className="p-4 flex flex-col justify-between h-[150px] text-center">
+                                            <h3 className="lg:text-lg text-md font-semibold mb-2">{blog.title}</h3>
+                                            <p className="text-orange-600 text-sm mb-2">
+                                                Author Name |{" "}
+                                                <span className="text-gray-600">{blog.date_posted}</span>
+                                            </p>
+                                            <p className="text-sm text-gray-600 line-clamp-3">
+                                                {blog.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
--- src/components/MiniBanner.jsx
+++ src/components/MiniBanner.jsx
@@ -1,3 +1,11 @@
-export default function MiniBanner(){
-    
+export default function MiniBanner({ bg_img, pageName }){
+    return(
+        <>
+            <section className="mini-banner" style={{backgroundImage: `url("${bg_img}")`}}>
+                <div className="wrapper w-full h-full flex flex-col justify-center items-center">
+                    <h1 className="relative z-10">{pageName}</h1>
+                </div>
+            </section>
+        </>
+    )
 }
