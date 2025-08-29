254c8c9cf639222d590239bcbc03a4dd1a312c97
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-08-29 10:04:51 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "img.freepik.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'html.kodesolution.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "html.kodesolution.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'mpg-backend-production.up.railway.app',
-        pathname: '/media/**',
+        protocol: "https",
+        hostname: "mpg-backend-production.up.railway.app",
+        pathname: "/media/**",
       },
       {
-        protocol: 'http',
-        hostname: 'mpg-backend-production.up.railway.app',
-        pathname: '/media/**',
+        protocol: "http",
+        hostname: "mpg-backend-production.up.railway.app",
+        pathname: "/media/**",
       },
       {
-        protocol: 'https',
-        hostname: 'backend.mpgstone.com',
-        pathname: '/media/**',
+        protocol: "https",
+        hostname: "backend.mpgstone.com",
+        pathname: "/media/**",
       },
     ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/product-category/:category/page/1",
+        destination: "/product-category/:category/",
+        permanent: true,
+      },
+      {
+        source: "/blogs/page/1",
+        destination: "/blogs/",
+        permanent: true,
+      },
+      {
+        source: "/products/page/1",
+        destination: "/products/",
+        permanent: true,
+      },
+    ];
   },
 };
 
